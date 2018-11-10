4215a28f471331f1e914f56b2ee9116f7d8fb95f
Seochokid <nicehero94@gmail.com>
2018-11-11 03:46:59 +0900

    Seat class에 변수추가, setter함수 수정, library class에 refresh함수 추가

--- library.cpp
+++ library.cpp
@@ -7,6 +7,17 @@
 using namespace std;
 
 library :: library() {
+	today = 0;
+	for(int i = 1; i < 11; i++) {
+		Study_room* tmp = new Study_room(i, 0);
+		srooms.push_back(tmp);
+	}
+	for(int i = 1; i < 4; i++) {
+		for(int j = 0; j < 50; j++) {
+			Seat* tmp = new Seat(i, j);
+			seats.push_back(tmp);
+		}
+	}
 	resource_data();
 	result();
 	int num = B.size();
@@ -23,6 +34,16 @@ library :: library() {
 	for(int i = 0; i < num; i++) {
 		delete E.back();
 		E.pop_back();
+	}
+	for(int i = 1; i < 11; i++) {
+		delete srooms.back();
+		srooms.pop_back();
+	}
+	for(int i = 1; i < 4; i++) {
+		for(int j = 0; j < 50; j++) {
+			delete seats.back();
+			seats.pop_back();
+		}
 	}
 }
 
@@ -116,7 +137,7 @@ string library :: check(int cnt, int date, string r_type, string r_name, string 
 			}
 		}
 	}
-	
+	refresh(date);
 	//return_code 1 : Non exist resource.
 	string output;
 	int flag = 0;
@@ -450,8 +471,8 @@ string library :: check(int cnt, int date, string r_type, string r_name, string 
 	return output;
 }
 
-string library :: check2(int cnt, int s_date, string s_type, int s_num, string s_op, string sm_type, string sm_name, int sm_num, int s_time) {
-
+string library :: check2(int cnt, int s_date, string s_type, int s_num, string s_op, string sm_type, string sm_name, int sm_num, int s_time, int hour) {
+	refresh(s_date);
 }
 
 void library :: Update(int date, string m_type, string m_name, string r_type, string r_name, int op) {
@@ -771,7 +792,7 @@ void library :: result() {
             fin2 >> sm_name;
             fin2 >> sm_num;
             fin2 >> s_time;
-            res += check2(cnt++, day, r_type, r_name, op, m_type, m_name);
+            res += check2(cnt++, day, s_type, stoi(s_num), s_op, sm_type, sm_name, stoi(sm_num), stoi(s_time), DtoHour(date));
         }
     }
     if(flag == 0) {
@@ -783,7 +804,7 @@ void library :: result() {
             fin2 >> sm_name;
             fin2 >> sm_num;
             fin2 >> s_time;
-            res += check2(cnt++, day, r_type, r_name, op, m_type, m_name);
+            res += check2(cnt++, day, s_type, stoi(s_num), s_op, sm_type, sm_name, stoi(sm_num), stoi(s_time), DtoHour(date));
             fin2 >> s_date;
         }
     } else if(flag == 1) {
@@ -810,4 +831,16 @@ void library :: print_member() {
 	for(auto s : U) cout << s->get_member() << endl;
 	for(auto s : G) cout << s->get_member() << endl;
 	for(auto s : F) cout << s->get_member() << endl;
+}
+
+void library :: refresh(int day) {
+	if(today != day) {
+		today = day;
+		for(auto s : srooms) {
+			s->set_Sroom('N', "", 0, 0);
+		}
+		for(auto s : seats) {
+			s->set_Seat('N', "", 0);
+		}
+	}
 }
--- library.h
+++ library.h
@@ -16,11 +16,13 @@ private:
 
 	vector<Study_room*> srooms;
 	vector<Seat*> seats;
+
+	int today;
 public:
 	library();
 	void resource_data();
 	string check(int cnt, int date, string r_type, string r_name, string op, string m_type, string m_name);
-	string check2(int cnt, int s_date, string s_type, int s_num, string s_op, string sm_type, string sm_name, int sm_num, int s_time);
+	string check2(int cnt, int s_date, string s_type, int s_num, string s_op, string sm_type, string sm_name, int sm_num, int s_time, int hour);
 	void Update(int date, string m_type, string m_name, string r_type, string r_name, int op);
 	bool is_registered(string name, string type);
 	int DtoInt(string date);
@@ -29,4 +31,5 @@ public:
 	void result();
 	void print_resource();
 	void print_member();
+	void refresh(int day);
 };
