eeada66c13080f091f60b55d38fc049db7bc967b
Seochokid <nicehero94@gmail.com>
2018-11-11 02:56:46 +0900

    DtoHour function 추가

--- library.h
+++ library.h
@@ -20,10 +20,12 @@ public:
 	library();
 	void resource_data();
 	string check(int cnt, int date, string r_type, string r_name, string op, string m_type, string m_name);
+	string check2(int cnt, int s_date, string s_type, int s_num, string s_op, string sm_type, string sm_name, int sm_num, int s_time);
 	void Update(int date, string m_type, string m_name, string r_type, string r_name, int op);
 	bool is_registered(string name, string type);
 	int DtoInt(string date);
 	string DtoString(int date);
+	int DtoHour(string date);
 	void result();
 	void print_resource();
 	void print_member();
